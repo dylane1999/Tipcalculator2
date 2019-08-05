ea9243c70f87d3a772ee2b2b82db8d2ff48e24e9
Nathan Engstrom <naengstrom@davidson.edu>
2019-08-04 20:12:06 -0700

    remove alerts, add html bars for calculations

---
+++ js/index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", help);
+
+function help() {
+  console.log("plzworkk");
+}
+
+function main() {
+  let bill = document.getElementById("bill").value;
+  let tipAsPercent = document.getElementById("tip").value;
+  let tipAsDollar = bill * tipAsPercent * 0.01;
+  let total = String(Number(bill) + Number(tipAsDollar));
+  let party = document.getElementById("party").value;
+  let perPerson = total / party;
+  document.getElementById("tipOutput").value = tipAsDollar;
+  document.getElementById("totalOutput").value = total;
+  document.getElementById("partyOutput").value = perPerson;
+
+  console.log(document.getElementById("party").value);
+  console.log(document.getElementById("bill").value);
+  console.log(document.getElementById("tip").value);
+}
