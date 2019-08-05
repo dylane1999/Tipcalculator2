c855254ced8f56b6b6200872825c316de916f329
dylane1999 <52979287+dylane1999@users.noreply.github.com>
2019-08-04 18:59:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-function nathan(){
-  return 
+document.addEventListener('DOMContentLoaded', help)
+
+
+function help(){
+console.log("plzworkk")
+}
+
+
+
+
+function main() {
+let bill = document.getElementById("bill").value;
+let tipAsPercent = document.getElementById("tip").value;
+let tipAsDollar = (bill * tipAsPercent) *.01;
+let total = String(Number(bill) + Number(tipAsDollar));
+let party = document.getElementById("party").value;
+let perPerson = total/party
+  console.log(document.getElementById("party").value);
+  console.log(document.getElementById("bill").value);
+  alert(`Total: ${total} \n
+   Total per Person: ${perPerson} \n 
+   Bill: ${bill} \n 
+   Tip: ${tipAsDollar}`)
   
+
+
 }
-
-
-document.getElementById("tipbutton").onclick(nathan())
-
-function party(people, total){
-    let amountPerPerson = (total/people)
-    return amountPerPerson;
-}
-
-
-
